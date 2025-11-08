2eb871bffe9ab6a6fbfd5a784e8b72b9b096c074
tristansinclair77 <tristansinclair77@gmail.com>
2025-11-08 05:01:09 -0500

    Added text/temp buttons to main menu as placeholder.

---
+++ Presentation/Views/Game/MainMenu.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace MonsterDungeon.Presentation.Views.Game
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : UserControl
+    {
+        public MainMenu()
+  {
+            InitializeComponent();
+        }
+    }
+}
